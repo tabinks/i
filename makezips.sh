6e119ec2489c4facfb581166791dd5bc52f26651
T. Andrew Binkowski <abinkowski@uchicago.edu>
2013-05-23 23:38:36 -0500

    Upate

--- makezips.sh
+++ makezips.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-rm -rf Issue1.zip Issue2.zip Issue3.zip
+rm -rf Issue0.zip Issue1.zip Issue2.zip Issue3.zip
 
+zip -j Issue1.zip Issue0/*
 zip -j Issue1.zip Issue1/*
 zip -j Issue2.zip Issue2/*
 zip -j Issue3.zip Issue3/*
